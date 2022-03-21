25b0b5a967113ef1f469c5d67e2b48030c04a027
Piyush Raj <piyushraj92739@gmail.com>
2022-03-21 18:37:45 +0530

    accounts and transaction verification

--- accounts.py
+++ accounts.py
@@ -1,31 +1,34 @@
-from Crypto.PublicKey import ECC
-from Crypto.Hash import keccak
-key = ECC.generate(curve='P-256')
-"""f = open('myprivatekey.pem','wt')
-f.write(key.export_key(format='OpenSSH'))
-f.close()
-f = open('myprivatekey.pem','rt')
-key = ECC.import_key(f.read())
-print(key)
-"""
+import ecdsa
+import json
+import base58
 class account:
     def __init__(self):
-        self.PrivateKey,self.PublicKey,self.Address = self.gen_key()
-        #self.
-    def gen_key(self):
-        temp = ECC.generate(curve='P-256')
-        private_key = (temp.export_key(passphrase="hi",format='DER',protection='PBKDF2WithHMAC-SHA1AndAES128-CBC').hex())
-        public_key = temp.public_key().export_key(format='DER').hex()
-        k = keccak.new(digest_bits=256)
-        k.update(bytes.fromhex(public_key))
-        Address = k.hexdigest()
-        
-        return ('0x'+private_key),('0x'+public_key),('0x'+Address)
-        
+        self.sk = ecdsa.SigningKey.generate(curve=ecdsa.NIST521p)
+        self.PrivateKey =  base58.b58encode(self.sk.to_string())
+        self.PublicKey = base58.b58encode(self.sk.verifying_key.to_string("uncompressed"))
+    def signTransaction(self,message):
+        sig = self.sk.sign(message)
+        if self.sk.verifying_key.verify(sig,message):
+            return base58.b58encode(sig)
+        else:
+            return False
+    def verfyTransaction(self,sig,message):
+        return self.sk.verifying_key.verify(base58.b58decode(sig),message)
 
+transac = {"sender": "A", "receiver": "B", "amount": 100, "timestamp": 1647690547.3355699}
 
+a = account()
+print(a.PrivateKey)
+print("-"*10)
+print(a.PublicKey)
+message = bytes(json.dumps(transac),'utf-8')
+print("-"*10)
+sig = a.signTransaction(message)
 
+print(sig)
 
+print("-"*10)
+print(a.verfyTransaction(sig,message))
 
 
 
