9be78c2b729fb7c746534cf6dd0efcc8c14cb200
Piyush Raj <piyushraj92739@gmail.com>
2022-03-28 12:35:21 +0530

    basic gossip

---
+++ node3/node.py
@@ -0,0 +1,16 @@
+import importlib.machinery
+import importlib.util
+
+
+# import the GossipNode class
+Gossip_loader = importlib.machinery.SourceFileLoader( 'Gossip', '../network/Gossip.py' )
+Gossip_spec = importlib.util.spec_from_loader( 'Gossip',Gossip_loader )
+Gossip = importlib.util.module_from_spec(Gossip_spec)
+Gossip_spec.loader.exec_module(Gossip)
+
+# port for this node
+port = 5010
+# ports for the nodes connected to this node
+connected_nodes = [5000, 5020]
+
+node = Gossip.GossipNode(port, connected_nodes)
